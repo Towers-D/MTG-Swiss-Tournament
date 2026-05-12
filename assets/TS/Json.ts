28529bc5bb3843d2f05666df43a35ff179c0ad67
David Towers <Towers.D27@gmail.com>
2026-05-12 21:32:30 +0100

    Created a  file to manage the JSON, currently it creates and checks whether it should expire (which is to be done on page load)

---
+++ assets/TS/Json.ts
@@ -0,0 +1,46 @@
+export type Json = { [key:string]: any};
+import { RowComponent } from "tabulator-tables";
+
+const JSON_STRING:string = "tournamentJSON";
+
+//TO-DO function to remove localstorage after lifespan
+
+/**
+ * Creates the initial JSON file needed to create rounds.
+ */
+export function createJSON(playerRows:Array<RowComponent>):void  {
+    const json:Json = {};
+    json.round = 0;
+    json.lifespan = Date.now() + 12 * 1000 * 60 * 60; // make nicer
+    json.players = {};
+
+    playerRows.forEach(row => {
+        const data = row.getData() as {
+            name: string;
+            remove: string;
+        }
+
+        json.players[data.name] = {};
+    });
+
+    localStorage.setItem(JSON_STRING, JSON.stringify(json));
+};
+
+/**
+ * Checks if the stored data exceeds the lifespan it was created with and if so deletes it.
+ * 
+ * @returns `null` when no localStorage is found. Otherwise return whether the data was removed. 
+ */
+export function checkExpired():boolean|null {
+    const raw = localStorage.getItem(JSON_STRING);
+    const data:Json|null = raw ? JSON.parse(raw) : null;
+
+    if (data) {
+        if (Date.now() > data.lifespan) {
+            localStorage.removeItem(JSON_STRING);
+            return true;
+        }
+        return false;
+    }
+    return null;
+}
