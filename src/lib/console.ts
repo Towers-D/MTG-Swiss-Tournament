b754ab107616488e2c028b062edce44d82ca0f81
Towers-D <Towers.D27@gmail.com>
2026-05-17 17:30:08 +0100

    Recreated index.html as tournment console page, with buttons that go to the other pages, delete and upload are waiting for implementation in {src/db/databases.ts}

---
+++ src/lib/console.ts
@@ -0,0 +1,27 @@
+import { isData } from "../db/database";
+
+function resetButtons(): void {
+    const CONSOLE_BUTTONS = document.querySelectorAll<HTMLButtonElement>(".consoleButton");
+
+    CONSOLE_BUTTONS.forEach(button => {
+        button.disabled = button.classList.contains('needsData') ? !isData() : isData();
+    })
+}
+
+function detectButtonPressed(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    if (target.tagName === 'BUTTON') {
+        console.log('Button clicked');
+        resetButtons();
+    }
+}
+
+export function setUp() {
+    resetButtons();
+
+    document.addEventListener("click", detectButtonPressed);
+    return () => {
+        document.removeEventListener('click', detectButtonPressed);
+    };
+}
---
+++ src/routes/console.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+    import { setUp } from "../lib/console";
+    import { deleteDatabase } from "../db/database";
+    import { goToPage } from "../lib/utils";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        setUp();
+    });
+</script>
+
+<h1>Tournament Console</h1>
+
+<div id='buttons'>
+    <button id="create" class="consoleButton" on:click={() => goToPage("lobby")}> Open Lobby </button>
+    <button id="continue" class="consoleButton needsData" on:click={() => goToPage("lobby")}> Continue Round </button>
+    <button id="standings" class="consoleButton needsData" on:click={() => goToPage("lobby")}> View Standings </button>
+    <button id="upload" class="consoleButton" on:click={() => console.log("json")}> Upload JSON </button>
+    <button id="delete" class="consoleButton needsData" on:click={deleteDatabase}> Delete Storage </button>
+</div>
