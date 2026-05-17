69030934ccbdb2999afe37a43397c3facacd1136
Towers-D <Towers.D27@gmail.com>
2026-05-17 17:48:20 +0100

    set header and redirect in lobby

--- src/lib/lobby.ts
+++ src/lib/lobby.ts
@@ -1,14 +1,23 @@
-import {createJSON, isData } from "./Json.js";
-import { PlayerTable } from "./PlayerTable.js";
+import { isData } from "../db/database";
+import { goToPage } from "./utils";
 
-document.addEventListener("DOMContentLoaded", () =>{
-    if (isData() === true) {
-        window.location.replace("/");
+
+// document.addEventListener("DOMContentLoaded", () =>{
+//     if (isData() === true) {
+//         window.location.replace("/");
+//     }
+
+//     let playerTable = new PlayerTable("players", "player_input", "add_player", "player_count");
+//     document.getElementById("gen_round_one")?.addEventListener("click", () => {
+//         createJSON(playerTable.table.getRows());
+//         window.location.assign("/round");
+//     });
+// });
+
+export function setUp() {
+    document.title = "MTG Swiss Lobby";
+    console.log('hello');
+    if (isData()) {
+        goToPage()
     }
-
-    let playerTable = new PlayerTable("players", "player_input", "add_player", "player_count");
-    document.getElementById("gen_round_one")?.addEventListener("click", () => {
-        createJSON(playerTable.table.getRows());
-        window.location.assign("/round");
-    });
-});
+}
--- src/routes/lobby.svelte
+++ src/routes/lobby.svelte
@@ -1,3 +1,12 @@
+<script lang='ts'>
+    import { setUp } from "../lib/lobby";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        setUp();
+    });
+</script>
+
 <h1>
     Tournament Lobby
 </h1>
