804ac8904dcad4e01618c8ca8e34ec2ea507ad49
Towers-D <Towers.D27@gmail.com>
2026-05-17 17:28:37 +0100

    Converting to svelte, created App.svelte to route to 'pages' to make SPA app for database reasons

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Router from "svelte-spa-router";
+    import Lobby from "./routes/lobby.svelte";
+    import Round from "./routes/round.svelte";
+    import Standings from "./routes/standings.svelte";
+    import Console from "./routes/console.svelte";
+</script>
+
+<nav>
+    <a href="/#/">Home</a>
+</nav>
+
+<Router routes = {{
+    '/': Console,
+    '/lobby': Lobby,
+    '/round': Round,
+    '/standings': Standings
+}}/>
