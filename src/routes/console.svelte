8686c76a9b3bc21cce071cb8716d81f406d9fa5c
David Towers <Towers.D27@gmail.com>
2026-05-30 20:26:48 +0100

    Console detects whether round has started when continuing round

--- src/routes/console.svelte
+++ src/routes/console.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-    import { dataExists, deleteDatabase, getPlayers } from "../db/database";
+    import { dataExists, deleteDatabase, getPlayers, hasRoundStarted } from "../db/database";
     import { goToButton, jsonButton, setUp } from "../lib/console";
     import { onMount, tick } from "svelte";
 
@@ -26,7 +26,7 @@
 <div id='buttons'>
     <button id="create" disabled={hasData} class="consoleButton" on:click={() => goToButton("lobby")}> Open Lobby </button>
     <!-- TODO continue should decide whether to go to creation or round based on the existence of the match -->
-    <button id="continue" disabled={!hasData} class="consoleButton" on:click={() => goToButton("round")}> Continue Round </button>
+    <button id="continue" disabled={!hasData} class="consoleButton" on:click={async () => goToButton(await hasRoundStarted() ? "round" : "create")}> Continue Round </button>
     <button id="standings" disabled={!hasData} class="consoleButton" on:click={() => goToButton("standings")}> View Standings </button>
     <button id="upload" disabled={hasData} class="consoleButton" on:click={jsonButton}> Upload JSON </button>
     <button id="view" disabled={hasData} class="consoleButton" on:click={async () =>{ await console.log(getPlayers()); await refresh();}}> Log Players </button>
