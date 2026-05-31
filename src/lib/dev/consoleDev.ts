b36f9c9902994c78006f5f7bbc4bc62862d8b6e4
JaScMiller <jascmiller@protonmail.com>
2026-06-01 00:16:15 +0100

    Delete and player initialise promise examples

--- src/lib/dev/consoleDev.ts
+++ src/lib/dev/consoleDev.ts
@@ -1,11 +1,11 @@
 import { addPlayer } from "../../db/database";
 
-export function initPlayers(){
-    addPlayer('Alice');
-    addPlayer('Bob');
-    addPlayer('Clare');
-    addPlayer('Daniel');
-    addPlayer('Eve');
-    addPlayer('Fred');
-    addPlayer('Georgia');
+export async function initPlayers(){
+    await addPlayer('Alice');
+    await addPlayer('Bob');
+    await addPlayer('Clare');
+    await addPlayer('Daniel');
+    await addPlayer('Eve');
+    await addPlayer('Fred');
+    await addPlayer('Georgia');
 }
